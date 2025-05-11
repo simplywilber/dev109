785428a0b0d9bc267a9e317714d1fb851a071e91
Wilber Amaya-Maurisio <107688732+simplywilber@users.noreply.github.com>
2025-05-10 18:58:09 -0700

    Add nested if else statement for input validation

--- m05/js/script.js
+++ m05/js/script.js
@@ -1,18 +1,22 @@
 let msg = `<h2>Multiplication Table</h2>`;
-let userNumber = prompt("Welcome! Please enter a number: ");
+let userNumber = prompt("Welcome! Please enter a number between 0-10: ");
 
-// Verifying the input is an integer
+// Validating the input
 if (isNaN(userNumber) || userNumber === null || userNumber === "") {
-    userNumber = 0;
+  userNumber = 0;
 } else {
+  if (userNumber >= 0 && userNumber <= 10) {
     userNumber = parseInt(userNumber);
+  } else {
+    userNumber = 0;
+  }
 }
 
-// Looping through nine iterations
+// Looping through iterations
 for (let i = 1; i <= 9; i++) {
-    let line = `${i} * ${userNumber} = ${i * userNumber}<br>`;
-    msg += line;
+  let line = `${i} * ${userNumber} = ${i * userNumber}<br>`;
+  msg += line;
 }
 
 // Final output
-document.getElementById('results').innerHTML = msg;
+document.getElementById("results").innerHTML = msg;
